b0dcca6d8a9e8c61ada6ec8ea1bd624de5658ba1
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-14 15:33:20 +0000

    [feat] adicionando a interface do repositorio do aluno

---
+++ server/src/core/interfaces/IAlunoRepository.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class IAlunoRepository(ABC):
+
+    @abstractmethod
+    def salva(self, aluno):
+        pass
+
+    @abstractmethod
+    def encontra_por_id(self, id: int):
+        pass
+
+    @abstractmethod
+    def encontra_por_email(self, email: str):
+        pass
