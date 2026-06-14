d27747c8efc2e18ecdf21d3c74dba64bc278d716
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-14 17:52:25 +0000

    [refactor] adicionando o mapper no materia

--- server/src/infra/repositories/SQLAlchemyMateriaRepository.py
+++ server/src/infra/repositories/SQLAlchemyMateriaRepository.py
@@ -1,14 +1,38 @@
 from core.entities.Materia import Materia
 from core.interfaces.IMateriaRepository import IMateriaRepository
-
+from infra.db.model.materia_model import MateriaModel
+from infra.mappers.MateriaMapper import MateriaMapper
 
 class SQLAlchemyMateriaRepositorio(IMateriaRepository):
     def __init__(self, session):
         self.session = session
 
-    def encontra_por_id(self,id_materia: int) -> Materia:
-        return (self.session.query(Materia).filter_by(id=id_materia).first())
+    def salva(self, materia: Materia) -> bool:
+        model = MateriaMapper.to_model(materia)
 
-    def retorna_todas_as_materias(self) -> list[Materia]:
-        return (self.session.query(Materia).all()
-        )
+        self.session.add(model)
+        self.session.commit()
+
+        return True
+
+    def encontra_por_id(self, id: int) -> Materia:
+        model = (self.session.query(MateriaModel).filter_by(id=id).first())
+
+        if not model:
+            return None
+
+        return MateriaMapper.to_entity(model)
+
+    def encontra_por_nome(self, nome: str) -> Materia:
+        model = (self.session.query(MateriaModel).filter_by(nome=nome).first())
+
+        if not model:
+            return None
+
+        return MateriaMapper.to_entity(model)
+
+    def lista_todas(self) -> list[Materia]:
+
+        models = (self.session.query(MateriaModel).all())
+
+        return [MateriaMapper.to_entity(model) for model in models]
