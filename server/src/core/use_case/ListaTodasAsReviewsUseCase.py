e1466d3541c1261d6e29e8e2c7dea68f9a30f248
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-14 16:17:51 +0000

    [feat] adicionando o listar todas as reviews

---
+++ server/src/core/use_case/ListaTodasAsReviewsUseCase.py
@@ -0,0 +1,25 @@
+from core.dto.ReviewDTO import ReviewDTO
+from core.interfaces.IReviewRepository import IReviewRepository
+from core.interfaces.IMateriaRepository import IMateriaRepository
+
+class ListaReviewsUserCase:
+
+    def __init__(self, repositorio_review: IReviewRepository, repositorio_materia: IMateriaRepository):
+        self.repositorio_review = repositorio_review
+        self.repositorio_materia = repositorio_materia
+
+    def executa(self, materia_id: int) -> list[ReviewDTO]:
+        materia = self.repositorio_materia.encontra_por_id(materia_id)
+
+        if not materia:
+            raise Exception("Matéria não encontrada")
+
+        reviews = (self.repositorio_review.encontra_por_materia(materia_id))
+
+        resultado = []
+
+        for review in reviews:
+            dto = ReviewDTO(comentario = review.comentario, nota = review.nota, id_aluno = review.id_aluno)
+            resultado.append(dto)
+
+        return resultado
