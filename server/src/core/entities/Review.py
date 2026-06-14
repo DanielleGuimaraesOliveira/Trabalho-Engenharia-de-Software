060d1eb4587ac5965d0aea71bd9f2a3e7c004a12
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-14 15:32:47 +0000

    [feat] adicionando a entidade review

---
+++ server/src/core/entities/Review.py
@@ -0,0 +1,24 @@
+from core.entities.Aluno import Aluno
+from core.entities.Materia import Materia
+
+
+class Review:
+
+    AVALIACAO_MINIMA = 1
+    AVALIACAO_MAXIMA = 5
+
+    def __init__(self, id: int, aluno: Aluno, materia: Materia, avaliacao: int, descricao: str =""):
+        self.__validaAvaliacao(avaliacao)
+
+        self.id = id
+        self.aluno = aluno
+        self.materia = materia
+        self.avaliacao = avaliacao
+        self.descricao = descricao
+
+    def __validaAvaliacao(self, avaliacao: int):
+        if avaliacao < self.AVALIACAO_MINIMA or avaliacao > self.AVALIACAO_MAXIMA:
+            raise ValueError(
+                f"A nota deve estar entre "
+                f"{self.AVALIACAO_MINIMA} e {self.AVALIACAO_MAXIMA}"
+            )
