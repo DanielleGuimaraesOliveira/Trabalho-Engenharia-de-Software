399207b696f741b8900d8fbc16bdc5fed5112b6c
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-14 16:40:10 +0000

    [feat] adicionando as funcoes do hash e do token de verificação

---
+++ server/src/infra/security.py/HashService.py
@@ -0,0 +1,13 @@
+from werkzeug.security import (
+    generate_password_hash,
+    check_password_hash
+)
+
+
+class HashService:
+
+    def hash(self, senha: str) -> str:
+        return generate_password_hash(senha)
+
+    def verificar(self, senha_hash: str, senha: str) -> bool:
+        return check_password_hash(senha_hash, senha)
---
+++ server/src/infra/security.py/JWTTokenService.py
@@ -0,0 +1,17 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+
+
+class JWTTokenService:
+    SECRET_KEY = "segredo"
+
+    def gerar_token(self, aluno_id: int) -> str:
+        payload = {
+            "sub": aluno_id,
+            "exp": datetime.now(timezone.utc()).ri + timedelta(hours=2)
+        }
+
+        return jwt.encode(payload,
+            self.SECRET_KEY,
+            algorithm="HS256"
+        )
