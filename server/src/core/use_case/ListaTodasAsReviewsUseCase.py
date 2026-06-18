607b367fb3e0db12fb25f2449b88716d11ed3dfe
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-18 18:33:43 +0000

    [fix] resolvendo o problema do get reviews

--- server/src/core/use_case/ListaTodasAsReviewsUseCase.py
+++ server/src/core/use_case/ListaTodasAsReviewsUseCase.py
@@ -14,32 +14,11 @@ class ListaReviewsUseCase:
         self.repositorio_review = repositorio_review
         self.repositorio_materia = repositorio_materia
 
-    def executa(self, materia_id: int) -> list[ReviewDTO]:
+    def executa(self, materia_id: int):
 
-        materia = (
-            self.repositorio_materia
-            .encontra_por_id(materia_id)
-        )
+        materia = self.repositorio_materia.encontra_por_id(materia_id)
 
         if not materia:
             raise Exception("Matéria não encontrada")
 
-        reviews = (
-            self.repositorio_review
-            .encontra_por_materia(materia_id)
-        )
-
-        resultado = []
-
-        for review in reviews:
-
-            dto = ReviewDTO(
-                comentario=review.comentario,
-                nota=review.nota,
-                id_aluno=review.id_aluno,
-                id_materia=review.id_materia
-            )
-
-            resultado.append(dto)
-
-        return resultado
+        return self.repositorio_review.encontra_por_materia(materia_id)
