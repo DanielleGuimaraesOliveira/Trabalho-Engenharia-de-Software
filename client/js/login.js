f1e4f7577af50180897bb7d28541bb3685e83c7c
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-18 23:25:20 +0000

    [feat] adicionando o login

---
+++ client/js/login.js
@@ -0,0 +1,38 @@
+const emailInput = document.getElementById("email");
+const senhaInput = document.getElementById("senha");
+const btnLogin = document.getElementById("btnLogin");
+
+const API_URL = `${window.location.protocol}//${window.location.hostname.replace(
+    "-5500",
+    "-5000"
+)}`;
+
+btnLogin.addEventListener("click", async () => {
+
+    const email = emailInput.value;
+    const senha = senhaInput.value;
+
+    try {
+        const response = await fetch(`${API_URL}/login`, { method: "POST", headers: { "Content-Type": "application/json"},
+        body: JSON.stringify({email, senha})
+        });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        localStorage.setItem("token", data.token);
+
+        window.location.href = "../index.html";
+
+    } else {
+
+        alert(data.message);
+    }
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Erro ao conectar com o servidor.");
+    }
+});
