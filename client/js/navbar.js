17a0268e49f35fd089b24553e311c9ef7b24f529
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-06-21 20:39:15 +0000

    [fix] consertando o bug

--- client/js/navbar.js
+++ client/js/navbar.js
@@ -1,24 +1,24 @@
 async function carregarNavbar() {
-
     const navbarPath =
         window.location.pathname.includes("/html/")
             ? "../navbar.html"
             : "navbar.html";
 
     const response = await fetch(navbarPath);
-
     const html = await response.text();
 
     document.getElementById("navbar").innerHTML = html;
 
+    console.log("Navbar carregada");
+
     const token = localStorage.getItem("token");
+    console.log("Token:", token);
 
-    if (token) {
-        const btnLogin = document.getElementById("btnLogin");
+    const btnLogin = document.getElementById("btnLogin");
+    console.log("Botão:", btnLogin);
 
-        if (btnLogin) {
-            btnLogin.remove();
-        }
+    if (token && btnLogin) {
+        btnLogin.remove();
     }
 }
 
